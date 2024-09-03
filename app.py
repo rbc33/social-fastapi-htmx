564b5b6e7c38b90dcb02d6a8163a76b8a2a2fb45
rbc33 <ricardobenthem@gmail.com>
2024-09-03 21:52:39 +0200

    lesson 2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from database import get_post, insert_post
-from models import Posts, Post
+from models import Posts, Post, UserPost
 from sqlite3 import Connection, Row
 
 app = FastAPI()
@@ -11,22 +11,35 @@ templates = Jinja2Templates(directory="templates")
 
 conn = Connection("social.db")
 conn.row_factory = Row
+user_id = 1
 
 
 @app.get("/")
 async def root(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse(request, "index.html", content={})
+    context = {
+        "posts": [
+            {
+                "post_title": "test",
+                "post_text": "text",
+            }
+        ]
+    }
+    post = get_post(conn)
+    return templates.TemplateResponse(request, "index.html", context=post.model_dump())
 
 
 @app.get("/posts")
-async def post() -> Posts:
-    return get_post(conn)
+async def post(request: Request) -> HTMLResponse:
+    posts = get_post(conn)
+    return templates.TemplateResponse(request, "posts.html", context=posts.model_dump())
 
 
 @app.post("/posts")
-async def add_post(post: Post) -> Post:
+async def add_post(post: UserPost, request: Request) -> HTMLResponse:
+    post = Post(user_id=user_id, **post.model_dump())
     insert_post(conn, post)
-    return post
+    posts = get_post(conn)
+    return templates.TemplateResponse(request, "posts.html", context=posts.model_dump())
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -2,9 +2,12 @@ from typing import List
 from pydantic import BaseModel
 
 
-class Post(BaseModel):
+class UserPost(BaseModel):
     post_title: str
     post_text: str
+
+
+class Post(UserPost):
     user_id: int
 
 
